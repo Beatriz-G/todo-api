44aca1e51c21750636a70da1c1c9d5ca62242ce0
beatriz_g <beatrizgue20@gmail.com>
2024-12-22 10:13:46 -0800

    Created add operation

--- todo-api/src/main/java/com/teamtreehouse/techdegrees/dao/Sql2oTodoDao.java
+++ todo-api/src/main/java/com/teamtreehouse/techdegrees/dao/Sql2oTodoDao.java
@@ -2,13 +2,31 @@ package com.teamtreehouse.techdegrees.dao;
 
 import com.teamtreehouse.techdegrees.exc.DaoException;
 import com.teamtreehouse.techdegrees.model.Todo;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
 public class Sql2oTodoDao implements TodoDao {
+    private final Sql2o sql2o;
+
+    public Sql2oTodoDao(Sql2o sql2o) {
+       this.sql2o = sql2o;
+   }
+
     @Override
     public void add(Todo todo) throws DaoException {
-        
+        String sql = "INSERT INTO todo(name, url) VALUES (:name, :ulr)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql)
+                    .bind(todo)
+                    .executeUpdate()
+                    .getKey();
+            todo.setId(id);
+        } catch (Sql2oException ex) {
+            throw new DaoException(ex, "Problem adding course");
+        }
     }
 
     @Override
